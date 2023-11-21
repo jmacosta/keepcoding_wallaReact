bb78a9ad9cc07d2d312c56a3fda831b54f396d14
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-11-21 22:42:53 +0100

    refactor files name

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react';
 import './App.css';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <LoginPage />
+    </>
+  );
 }
 
 export default App;
--- src/pages/LoginPage/LoginPage.js
+++
@@ -1,2 +0,0 @@
-function LoginPage() {}
-export default LoginPage;
