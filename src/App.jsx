b4aaea8d1cd5fe6c1542ac3ffd9c9f554836039a
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-11-26 23:09:36 +0100

    Add Rpoutes to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import { useState } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Layout from './Components/Layout/Layout';
+import AdvertPage from './pages/AdvertPage';
+import AdvertsPages from './pages/AdvertsPages';
 import LoginPage from './pages/LoginPage/LoginPage';
+import NewAdvertPage from './pages/NewAdvertPage';
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <LoginPage />;
+  return (
+    <Routes>
+      <Route path='/login' element={<LoginPage />}></Route>
+      <Route path='/adverts' element={<Layout />}>
+        <Route index element={<AdvertsPages />} />
+        <Route path=':adverId' element={<AdvertPage />} />
+        <Route path='new' element={<NewAdvertPage />} />
+      </Route>
+      <Route path='/' element={<Navigate to='/adverts' />} />
+      <Route path='/404' element={<div>404 | Not found</div>} />
+      <Route path='*' element={<Navigate to='/404' />} />
+    </Routes>
+  );
 }
 
 export default App;
