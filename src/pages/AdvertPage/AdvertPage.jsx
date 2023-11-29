9074f7ab3c2004fe7be59333659e8a0c1a3f631e
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-11-29 20:27:40 +0100

    WIP AdvertPage css working

--- src/pages/AdvertPage/AdvertPage.jsx
+++ src/pages/AdvertPage/AdvertPage.jsx
@@ -2,7 +2,14 @@ import { useState } from 'react';
 import arrowBack from '../../assets/arrowBack.svg';
 import noImage from '../../assets/no-image.jpg';
 import './advertPage.css';
-function AdvertPage(product) {
+function AdvertPage() {
+  const product = {
+    name: 'Sell Thing',
+    description: 'Description thing',
+    image: 'no-image.jpg',
+    price: 50,
+    sellOrSearch: true
+  };
   const [imageError, setImageError] = useState(false);
   return (
     <main>
@@ -23,22 +30,25 @@ function AdvertPage(product) {
             />
           </div>
         </section>
-
-        <div className='photo'>
-          <img
-            src={imageError ? noImage : `../../assets/${product.image}`}
-            onError={() => {
-              setImageError(true);
-            }}
-          />
-        </div>
-        <div className='description'>
-          <h2>${product.name}</h2>
-          <h4>${product.tags}</h4>
-          <h1>${product.price}€</h1>
-          <p>${product.description}</p>
-          <div id='authorized_buttons'></div>
-        </div>
+        <section id='card__content'>
+          <div className='photo'>
+            <img
+              src={imageError ? noImage : `../../assets/${product.image}`}
+              onError={() => {
+                setImageError(true);
+              }}
+            />
+          </div>
+          <div className='description'>
+            <h2>{product.name}</h2>
+            <h4>{product.tags}</h4>
+            <h1>{product.price}€</h1>
+            <p>{product.description}</p>
+            <div id='authorized_buttons'>
+              <button>Borrar</button>
+            </div>
+          </div>
+        </section>
       </section>
     </main>
   );
