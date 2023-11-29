d033bf44879cd64477729e431cf4381ffe91ade9
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-11-29 19:52:09 +0100

    WIP advertPage

--- src/pages/AdvertPage/AdvertPage.jsx
+++ src/pages/AdvertPage/AdvertPage.jsx
@@ -1,53 +1,46 @@
+import { useState } from 'react';
+import arrowBack from '../../assets/arrowBack.svg';
+import noImage from '../../assets/no-image.jpg';
+import './advertPage.css';
 function AdvertPage(product) {
+  const [imageError, setImageError] = useState(false);
   return (
-    <>
-      <section id='header_product_detail'>
-        <nav>
-          <button id='go_home'>
-            <svg
-              class='arrow'
-              version='1.1'
-              viewBox='0 0 512 512'
-              width='512px'
-              xml:space='preserve'
-              xmlns='http://www.w3.org/2000/svg'
-              xmlns:xlink='http://www.w3.org/1999/xlink'
-            >
-              <polygon
-                points='352,115.4 331.3,96 160,256 331.3,416 352,396.7 201.5,256 '
-                stroke='#727272'
-              />
-            </svg>
-            <span>Volver a inicio</span>
-          </button>
-        </nav>
-        <div class='sell_or_search'>
-          <img
-            src='${sellOrSeachIcon.image}'
-            alt='${
+    <main>
+      <section className='card'>
+        <section id='header_product_detail'>
+          <nav>
+            <button id='go_home'>
+              <img className='arrow' src={arrowBack} />
+              <span>Volver a inicio</span>
+            </button>
+          </nav>
+          <div className='sell_or_search'>
+            <img
+              src='${sellOrSeachIcon.image}'
+              alt='${
     sellOrSeachIcon.alt
   }'
+            />
+          </div>
+        </section>
+
+        <div className='photo'>
+          <img
+            src={imageError ? noImage : `../../assets/${product.image}`}
+            onError={() => {
+              setImageError(true);
+            }}
           />
         </div>
+        <div className='description'>
+          <h2>${product.name}</h2>
+          <h4>${product.tags}</h4>
+          <h1>${product.price}€</h1>
+          <p>${product.description}</p>
+          <div id='authorized_buttons'></div>
+        </div>
       </section>
-
-      <div class='photo'>
-        <img
-          src='${
-      product.image
-    }'
-          onerror="this.src='assets/no-image.jpg';"
-          alt='${product.name}'
-        />
-      </div>
-      <div class='description'>
-        <h2>${product.name}</h2>
-        <h4>${product.tags.join(', ')}</h4>
-        <h1>${product.price}€</h1>
-        <p>${product.description}</p>
-        <div id='authorized_buttons'></div>
-      </div>
-    </>
+    </main>
   );
 }
 export default AdvertPage;
