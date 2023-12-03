bc9e5445c1246a0e2f35b584db415b19574e39ad
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-12-03 21:54:48 +0100

    add authorization function

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
-const client = axios.create({
+export const client = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL
 });
 client.interceptors.response.use(response => response.data);
+
+export const setAuthorizationHeader = token =>
+  (client.defaults.headers.common['Authorization'] = `Bearer ${token}`);
+export const removeAuthorizationHeader = () => {
+  delete client.defaults.headers.common['Authorization'];
+};
 export default client;
