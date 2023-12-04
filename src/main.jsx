fa2092508a51ba32db952b6c729f064b72192c11
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-12-04 21:09:54 +0100

    Feat: Add accestoken to init app

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import { setAuthorizationHeader } from './api/client.js';
 import './index.css';
+import storage from './utils/storage.js';
+const accessToken = storage.get('auth');
+if (accessToken) {
+  setAuthorizationHeader(accessToken);
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <App iniatiallyLogged={!!accessToken} />
     </BrowserRouter>
   </React.StrictMode>
 );
