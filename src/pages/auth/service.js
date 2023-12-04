e732aa98bac46de43a1602017cb70ceb14df7307
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-12-04 21:13:37 +0100

    Feat: Add save accessToken to localStorage

--- src/pages/auth/service.js
+++ src/pages/auth/service.js
@@ -1,6 +1,14 @@
 import { client, setAuthorizationHeader } from '../../api/client';
+import storage from '../../utils/storage';
 export const login = userLoginInfo => {
+  const { rememberMe } = userLoginInfo;
   return client
     .post('/api/auth/login', userLoginInfo)
-    .then(({ accesToken }) => setAuthorizationHeader(accesToken));
+    .then(({ accessToken }) => {
+      setAuthorizationHeader(accessToken);
+      // to do manage the state for if persisted
+      if (rememberMe) {
+        storage.set('auth', accessToken);
+      }
+    });
 };
