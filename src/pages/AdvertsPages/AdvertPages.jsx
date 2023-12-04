4c40341654aaef6f48ba90c47e2ce9f3f13d1bc8
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-12-04 22:42:23 +0100

    WIP First approach to show adverts

--- src/pages/AdvertsPages/AdvertPages.jsx
+++ src/pages/AdvertsPages/AdvertPages.jsx
@@ -1,53 +1,51 @@
 import { useEffect, useState } from 'react';
+import { Button } from '../../Components/sharedComponents/Button.jsx';
 import { getLatestAdverts } from '../../api/service.js';
 import magnifyGlass from '../../assets/magnify-glass.svg';
 import noImage from '../../assets/no-image.jpg';
 import { sellSearchIcon } from '../../utils/sellSearchIcon.js';
+
 import './advertpages.css';
 
 const AdvertPages = () => {
   const [adverts, setAdverts] = useState([]);
-  const product = {
-    name: 'Sell Thing',
-    description: 'Description thing',
-    image: 'no-image.jpg',
-    price: 50,
-    sellOrSearch: false
-  };
+
   useEffect(() => {
     getLatestAdverts().then(adverts => setAdverts(adverts));
   }, []);
 
   const [imageError, setImageError] = useState(false);
-  return (
+  // to do create a component who receives a object with date to show
+  return adverts.length ? (
     <a href='/product_detail.html?id=${product.id}&_expand=user'>
       <div className='card'>
         <figure>
           <div className='sell_or_search'>
             <img
-              src={sellSearchIcon(product.sellOrSearch).image}
-              alt={sellSearchIcon(product.sellOrSearch).alt}
+              src={sellSearchIcon(adverts[3].sale).image}
+              alt={sellSearchIcon(adverts[3].sale).alt}
             />
           </div>
           <img
             className='product_image'
-            src={imageError ? noImage : `../../assets/${product.image}`}
+            src={imageError ? noImage : `${adverts[3].photo}`}
             onError={() => {
               setImageError(true);
             }}
-            alt={product.name}
+            alt={adverts[3].name}
           />
         </figure>
         <div className='text-content'>
-          <div className='product_name'>{product.name}</div>
-          <div className='product_description'>{product.description}</div>
-          <div className='product_price'>{product.price}€</div>
+          <div className='product_name'>{adverts[3].name}</div>
+          <div className='product_price'>{adverts[3].price}€</div>
         </div>
         <div className='overlay'>
           <img src={magnifyGlass} />
         </div>
       </div>
     </a>
+  ) : (
+    <Button>Crea tu primer anuncio...</Button>
   );
 };
 export default AdvertPages;
