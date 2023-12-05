b8a823ce8663e15a4d5d96d73b726321d9e05822
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-12-05 18:58:32 +0100

    Feat: refactor advertList card component

---
+++ src/pages/AdvertsPages/AdvertListCard.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import magnifyGlass from '../../assets/magnify-glass.svg';
+import noImage from '../../assets/no-image.jpg';
+import { sellSearchIcon } from '../../utils/sellSearchIcon.js';
+export const AdvertListCard = ({ sale, photo, name, price, tags }) => {
+  const [imageError, setImageError] = useState(false);
+
+  return (
+    <a href='/product_detail.html?id=${product.id}&_expand=user'>
+      <div className='card'>
+        <figure>
+          <div className='sell_or_search'>
+            <img
+              src={sellSearchIcon(sale).image}
+              alt={sellSearchIcon(sale).alt}
+            />
+          </div>
+          <img
+            className='product_image'
+            src={imageError ? noImage : `${photo}`}
+            onError={() => {
+              setImageError(true);
+            }}
+            alt={name}
+          />
+        </figure>
+        <div className='text-content'>
+          <div className='product_name'>{name}</div>
+          <h5>{tags.join(' ')}</h5>
+          <div className='product_price'>{price}€</div>
+        </div>
+        <div className='overlay'>
+          <img src={magnifyGlass} />
+        </div>
+      </div>
+    </a>
+  );
+};
--- src/pages/AdvertsPages/AdvertPages.jsx
+++ src/pages/AdvertsPages/AdvertPages.jsx
@@ -1,9 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Button } from '../../Components/sharedComponents/Button.jsx';
 import { getLatestAdverts } from '../../api/service.js';
-import magnifyGlass from '../../assets/magnify-glass.svg';
-import noImage from '../../assets/no-image.jpg';
-import { sellSearchIcon } from '../../utils/sellSearchIcon.js';
+import { AdvertListCard } from './AdvertListCard.jsx';
 
 import './advertpages.css';
 
@@ -14,36 +12,10 @@ const AdvertPages = () => {
     getLatestAdverts().then(adverts => setAdverts(adverts));
   }, []);
 
-  const [imageError, setImageError] = useState(false);
   // to do create a component who receives a object with date to show
+  console.log(adverts);
   return adverts.length ? (
-    <a href='/product_detail.html?id=${product.id}&_expand=user'>
-      <div className='card'>
-        <figure>
-          <div className='sell_or_search'>
-            <img
-              src={sellSearchIcon(adverts[3].sale).image}
-              alt={sellSearchIcon(adverts[3].sale).alt}
-            />
-          </div>
-          <img
-            className='product_image'
-            src={imageError ? noImage : `${adverts[3].photo}`}
-            onError={() => {
-              setImageError(true);
-            }}
-            alt={adverts[3].name}
-          />
-        </figure>
-        <div className='text-content'>
-          <div className='product_name'>{adverts[3].name}</div>
-          <div className='product_price'>{adverts[3].price}€</div>
-        </div>
-        <div className='overlay'>
-          <img src={magnifyGlass} />
-        </div>
-      </div>
-    </a>
+    adverts.map(advert => <AdvertListCard {...advert} />)
   ) : (
     <Button>Crea tu primer anuncio...</Button>
   );
