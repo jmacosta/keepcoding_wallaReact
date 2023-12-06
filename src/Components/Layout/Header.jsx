e141f3d682111b4bb4664e80b405db5addc6f7e4
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-12-06 22:51:56 +0100

    Feat: Add Links

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -1,15 +1,26 @@
+import { Link, NavLink } from 'react-router-dom';
 import logo from '../../assets/logo_horizontal.svg';
+import { UseAuth } from '../../pages/auth/context';
 import './header.css';
 const Header = ({ className }) => {
+  const { isLogged, onLogout } = UseAuth;
   return (
     <header className={className}>
       <div className='logo'>
-        <img src={logo} />
+        <Link to='/adverts'>
+          <img src={logo} />
+        </Link>
       </div>
       <nav>
         <ul>
-          <li>adverts</li>
-          <li>NewAdvert</li>
+          <li>
+            <NavLink to='/adverts' end>
+              Ver últimos anuncios
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/adverts/new'>Crear anuncio</NavLink>
+          </li>
           <li>Logout</li>
         </ul>
       </nav>
