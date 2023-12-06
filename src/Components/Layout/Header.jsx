e4d11ddf826f2629cee0e5125d2ca8a33061a4ea
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-12-06 15:07:23 +0100

    Feat: layout for nav and logo elements

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -1,4 +1,19 @@
-const Header = () => {
-  return <header className='header'>Meteremos Header aqui</header>;
+import logo from '../../assets/logo_horizontal.svg';
+import './header.css';
+const Header = ({ className }) => {
+  return (
+    <header className={className}>
+      <div className='logo'>
+        <img src={logo} />
+      </div>
+      <nav>
+        <ul>
+          <li>adverts</li>
+          <li>NewAdvert</li>
+          <li>Logout</li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 export default Header;
