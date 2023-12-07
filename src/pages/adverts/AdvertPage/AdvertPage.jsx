d88ab6b0d8e3d5933612595742f761785ab9f8c8
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-12-07 20:52:32 +0100

    styles: fix advertstyle

--- src/pages/adverts/AdvertPage/AdvertPage.jsx
+++ src/pages/adverts/AdvertPage/AdvertPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import arrowBack from '../../../assets/arrowBack.svg';
 import noImage from '../../../assets/no-image.jpg';
+import sellIcon from '../../../assets/sell_icon.svg';
 import './advertPage.css';
 function AdvertPage() {
   const product = {
@@ -12,26 +12,13 @@ function AdvertPage() {
   };
   const [imageError, setImageError] = useState(false);
   return (
-    <main>
-      <section className='card'>
-        <section id='header_product_detail'>
-          <nav>
-            <button id='go_home'>
-              <img className='arrow' src={arrowBack} />
-              <span>Volver a inicio</span>
-            </button>
-          </nav>
-          <div className='sell_or_search'>
-            <img
-              src='${sellOrSeachIcon.image}'
-              alt='${
-    sellOrSeachIcon.alt
-  }'
-            />
-          </div>
-        </section>
-        <section id='card__content'>
-          <div className='photo'>
+    <main className='detail__advert mainCard '>
+      <div className='modal'>
+        <div className='modal__header'>
+          <span className='modal__title'>{product.name}</span>
+        </div>
+        <div className='modal__body'>
+          <div className='modal-content photo'>
             <img
               src={imageError ? noImage : `../../assets/${product.image}`}
               onError={() => {
@@ -39,18 +26,107 @@ function AdvertPage() {
               }}
             />
           </div>
-          <div className='description'>
-            <h2>{product.name}</h2>
-            <h4>{product.tags}</h4>
-            <h1>{product.price}€</h1>
-            <p>{product.description}</p>
-            <div id='authorized_buttons'>
-              <button>Borrar</button>
+          {/* <!-- Price of Product --> */}
+          <div className='modal-content'>
+            <span className='price'>{`${product.price}€`}</span>
+          </div>
+
+          <div className='modal-content'>
+            <label className='modal-content__label' htmlFor='price'>
+              tags
+            </label>
+
+            <p className='modal-content'>
+              Precio en € de venta / compra máximo para tu producto
+            </p>
+          </div>
+          {/* Search / sell Product  */}
+
+          <div className='radio-tile-group'>
+            <div className='input-container'>
+              <input
+                id='sell'
+                className='radio-button'
+                type='radio'
+                name='sellOrSearch'
+                required
+                value='true'
+              />
+              <div className='radio-tile'>
+                <div className='icon sell-icon'>
+                  <img src={sellIcon} />
+                </div>
+                <label htmlFor='sell' className='radio-tile-label'>
+                  Venta
+                </label>
+              </div>
+            </div>
+
+            <div className='input-container'>
+              <input
+                id='search'
+                className='radio-button'
+                type='radio'
+                name='sellOrSearch'
+                required
+                value='false'
+              />
+              <div className='radio-tile'>
+                <div className='icon search-icon'></div>
+                <label htmlFor='search' className='radio-tile-label'>
+                  Busqueda
+                </label>
+              </div>
             </div>
           </div>
-        </section>
-      </section>
+        </div>
+        <div className='modal__footer'>
+          <button className='button button--primary' id='productCreateButton'>
+            Borrar Anuncio
+          </button>
+        </div>
+      </div>
     </main>
+
+    //   <main className='mainCard'>
+    //     <section className='card'>
+    //       <section id='header_product_detail'>
+    //         <nav>
+    //           <button id='go_home'>
+    //             <img className='arrow' src={arrowBack} />
+    //             <span>Volver a inicio</span>
+    //           </button>
+    //         </nav>
+    //         <div className='sell_or_search'>
+    //           <img
+    //             src='${sellOrSeachIcon.image}'
+    //             alt='${
+    //   sellOrSeachIcon.alt
+    // }'
+    //           />
+    //         </div>
+    //       </section>
+    //       <section id='card__content'>
+    //         <div className='photo'>
+    //           <img
+    //             src={imageError ? noImage : `../../assets/${product.image}`}
+    //             onError={() => {
+    //               setImageError(true);
+    //             }}
+    //           />
+    //         </div>
+    //         <div className='description'>
+    //           <h2>{product.name}</h2>
+    //           <h4>{product.tags}</h4>
+    //           <h1>{product.price}€</h1>
+    //           <p>{product.description}</p>
+    //           <div id='authorized_buttons'>
+    //             <button>Borrar</button>
+    //           </div>
+    //         </div>
+    //       </section>
+    //     </section>
+    //   </main>
   );
 }
 export default AdvertPage;
--- src/pages/adverts/NewAdvertPage/NewAdvertPage.jsx
+++ src/pages/adverts/NewAdvertPage/NewAdvertPage.jsx
@@ -5,113 +5,108 @@ function NewAdvertPage() {
   return (
     <main className='mainCard'>
       <form id='product_create'>
-        <div className='container'>
-          <div className='modal'>
-            <div className='modal__header'>
-              <span className='modal__title'>Nuevo producto</span>
+        <div className='modal'>
+          <div className='modal__header'>
+            <span className='modal__title'>Nuevo producto</span>
+          </div>
+          <div className='modal__body'>
+            {/* <!-- Name of Product --> */}
+            <div className='input'>
+              <label className='input__label' htmlFor='name'>
+                Nombre
+              </label>
+              <input
+                id='name'
+                className='input__field'
+                type='text'
+                name='name'
+                required
+                autoComplete='off'
+                title='Por favor rellena este campo'
+              />
+              <p className='input__description'>
+                El nombre tiene un limite de 32 caracteres
+              </p>
             </div>
-            <div className='modal__body'>
-              {/* <!-- Name of Product --> */}
-              <div className='input'>
-                <label className='input__label' htmlFor='name'>
-                  Nombre
-                </label>
-                <input
-                  id='name'
-                  className='input__field'
-                  type='text'
-                  name='name'
-                  required
-                  autoComplete='off'
-                  title='Por favor rellena este campo'
-                />
-                <p className='input__description'>
-                  El nombre tiene un limite de 32 caracteres
-                </p>
-              </div>
 
-              {/* <!-- Image of product --> */}
-              <div className='input'>
-                <label className='input__label' htmlFor='image'>
-                  Imagen
-                </label>
-                <input
-                  className='input__field'
-                  type='file'
-                  name='image'
-                  id='image'
-                />
-                <p className='input__description'>
-                  Introduce una url válida con la foto de tu producto
-                </p>
-              </div>
-              {/* <!-- Price of Product --> */}
-              <div className='input'>
-                <label className='input__label' htmlFor='price'>
-                  Precio
-                </label>
-                <input
-                  className='input__field'
-                  type='number'
-                  min='0'
-                  required
-                  name='price'
-                  id='price'
-                />
-                <p className='input__description'>
-                  Precio en € de venta / compra máximo para tu producto
-                </p>
-              </div>
-              {/* Search / sell Product  */}
+            {/* <!-- Image of product --> */}
+            <div className='input'>
+              <label className='input__label' htmlFor='image'>
+                Imagen
+              </label>
+              <input
+                className='input__field'
+                type='file'
+                name='image'
+                id='image'
+              />
+              <p className='input__description'>
+                Introduce una url válida con la foto de tu producto
+              </p>
+            </div>
+            {/* <!-- Price of Product --> */}
+            <div className='input'>
+              <label className='input__label' htmlFor='price'>
+                Precio
+              </label>
+              <input
+                className='input__field'
+                type='number'
+                min='0'
+                required
+                name='price'
+                id='price'
+              />
+              <p className='input__description'>
+                Precio en € de venta / compra máximo para tu producto
+              </p>
+            </div>
+            {/* Search / sell Product  */}
 
-              <div className='radio-tile-group'>
-                <div className='input-container'>
-                  <input
-                    id='sell'
-                    className='radio-button'
-                    type='radio'
-                    name='sellOrSearch'
-                    required
-                    value='true'
-                  />
-                  <div className='radio-tile'>
-                    <div className='icon sell-icon'>
-                      <img src={sellIcon} />
-                    </div>
-                    <label htmlFor='sell' className='radio-tile-label'>
-                      Venta
-                    </label>
+            <div className='radio-tile-group'>
+              <div className='input-container'>
+                <input
+                  id='sell'
+                  className='radio-button'
+                  type='radio'
+                  name='sellOrSearch'
+                  required
+                  value='true'
+                />
+                <div className='radio-tile'>
+                  <div className='icon sell-icon'>
+                    <img src={sellIcon} />
                   </div>
+                  <label htmlFor='sell' className='radio-tile-label'>
+                    Venta
+                  </label>
                 </div>
+              </div>
 
-                <div className='input-container'>
-                  <input
-                    id='search'
-                    className='radio-button'
-                    type='radio'
-                    name='sellOrSearch'
-                    required
-                    value='false'
-                  />
-                  <div className='radio-tile'>
-                    <div className='icon search-icon'>
-                      <img src={searchIcon} />
-                    </div>
-                    <label htmlFor='search' className='radio-tile-label'>
-                      Busqueda
-                    </label>
+              <div className='input-container'>
+                <input
+                  id='search'
+                  className='radio-button'
+                  type='radio'
+                  name='sellOrSearch'
+                  required
+                  value='false'
+                />
+                <div className='radio-tile'>
+                  <div className='icon search-icon'>
+                    <img src={searchIcon} />
                   </div>
+                  <label htmlFor='search' className='radio-tile-label'>
+                    Busqueda
+                  </label>
                 </div>
               </div>
             </div>
-            <div className='modal__footer'>
-              <button
-                className='button button--primary'
-                id='productCreateButton'
-              >
-                Crear producto
-              </button>
-            </div>
+          </div>
+          <div className='modal__footer'>
+            <button className='button button--primary' id='productCreateButton'>
+              Crear producto
+            </button>
           </div>
         </div>
       </form>
