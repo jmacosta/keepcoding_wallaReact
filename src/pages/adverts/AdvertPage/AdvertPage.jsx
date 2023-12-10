0ea0d3ce95ebf158f220e8c348fac34617ce03dd
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-12-11 00:32:14 +0100

    feat:redirect 404

--- src/pages/adverts/AdvertPage/AdvertPage.jsx
+++ src/pages/adverts/AdvertPage/AdvertPage.jsx
@@ -1,17 +1,23 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { getAdvert } from '../../../api/service';
 import './advertPage.css';
 import { AdvertDetailCard } from './components/AdvertDetailCard';
 
 function AdvertPage() {
   const { advertId } = useParams();
+  const navigate = useNavigate();
 
-  // const advertId = 'bb37771a-5832-4234-8021-79f8118d893a';
   const [advert, setAdvert] = useState([]);
   useEffect(() => {
-    getAdvert(advertId).then(advert => setAdvert(advert));
-  }, [advertId]);
+    getAdvert(advertId)
+      .then(advert => setAdvert(advert))
+      .catch(error => {
+        if (error.status === 404) {
+          navigate('/404');
+        }
+      });
+  }, [navigate, advertId]);
 
   return <AdvertDetailCard key={advertId} advert={advert} />;
 }
