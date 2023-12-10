d765a463f037331120db95554fd97fdc41dc79a3
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-12-10 22:32:28 +0100

    feat:add confirmed logout

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import logo from '../../assets/logo_horizontal.svg';
 import { UseAuth } from '../../pages/auth/context';
+import { ConfirmComponent } from '../sharedComponents/ConfirmComponent';
 import './header.css';
 const Header = ({ className }) => {
   const { isLogged, onLogout } = UseAuth();
+  const [confirm, setconfirmed] = useState(false);
+  const confirmedLogout = () => {
+    setconfirmed(true);
+  };
+  const resetFunction = () => {
+    setconfirmed(false);
+  };
   return (
     <header className={'header'}>
       <div className='logo'>
@@ -20,11 +29,16 @@ const Header = ({ className }) => {
           <NavLink to='/adverts/new'>
             <li>Crear anuncio</li>
           </NavLink>
-          <li className='logout' onClick={onLogout}>
+          <li className='logout' onClick={confirmedLogout}>
             Logout
           </li>
         </ul>
       </nav>
+      {confirm && (
+        <ConfirmComponent execFunction={onLogout} resetFunction={resetFunction}>
+          ¿Está seguro que desea desconectar?
+        </ConfirmComponent>
+      )}
     </header>
   );
 };
